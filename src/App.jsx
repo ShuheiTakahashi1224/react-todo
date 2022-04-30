4afe4b2d53709c6bba111e8f0d566bd3e6e18993
ShuheiTakahashi1224 <83751748+ShuheiTakahashi1224@users.noreply.github.com>
2022-04-30 21:56:19 +0900

    update : task 追加機能 実装

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,63 @@
+import { useState } from "react";
 import "./styles.css";
 
 const App = () => {
+  const [todoText, setTodoText] = useState("");
+  const [incompleteTodoList, setIncompleteTodoList] = useState([
+    "ああああああ",
+    "いいいいいいい"
+  ]);
+  const [completeTodoList, setCompleteTodoList] = useState([
+    "ううううううう",
+    "えええええええ"
+  ]);
+
+  const changeTodoText = (event) => setTodoText(event.target.value);
+  const addTask = () => {
+    if (todoText === "") return;
+    const newTodoList = [...incompleteTodoList, todoText];
+    setIncompleteTodoList(newTodoList);
+    setTodoText("");
+  };
   return (
     <>
       <div className="c-task p-add_task">
-        <input type="text" placeholder="TODOを入力" className="c-input" />
-        <button className="c-button">追加</button>
+        <input
+          type="text"
+          placeholder="TODOを入力"
+          value={todoText}
+          className="c-input"
+          onChange={(event) => changeTodoText(event)}
+        />
+        <button onClick={() => addTask()} className="c-button">
+          追加
+        </button>
       </div>
       <div className="c-task p-incomplete_task">
         <p className="c-title">未完了のTODO</p>
         <ul className="c-task_list">
-          <li className="c-task_list__item">
-            <p className="c-task_list__item_name">ああああああああ</p>
-            <button className="c-button">完了</button>
-            <button className="c-button">削除</button>
-          </li>
-          <li className="c-task_list__item">
-            <p className="c-task_list__item_name">いいいいいいいいいい</p>
-            <button className="c-button">完了</button>
-            <button className="c-button">削除</button>
-          </li>
+          {incompleteTodoList.map((todo) => {
+            return (
+              <li key={todo} className="c-task_list__item">
+                <p className="c-task_list__item_name">{todo}</p>
+                <button className="c-button">完了</button>
+                <button className="c-button">削除</button>
+              </li>
+            );
+          })}
         </ul>
       </div>
       <div className="c-task p-complete_task">
         <p className="c-title">未完了のTODO</p>
         <ul className="c-task_list">
-          <li className="c-task_list__item">
-            <p className="c-task_list__item_name">うううううううううう</p>
-            <button className="c-button">戻す</button>
-          </li>
+          {completeTodoList.map((todo) => {
+            return (
+              <li key={todo} className="c-task_list__item">
+                <p className="c-task_list__item_name">{todo}</p>
+                <button className="c-button">戻す</button>
+              </li>
+            );
+          })}
         </ul>
       </div>
     </>
