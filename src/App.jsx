e823ee08cf4c8a7acbaa2590a1b5d284f650619a
ShuheiTakahashi1224 <83751748+ShuheiTakahashi1224@users.noreply.github.com>
2022-04-30 22:03:34 +0900

    update : task 削除機能 実装

--- src/App.jsx
+++ src/App.jsx
@@ -19,6 +19,12 @@ const App = () => {
     setIncompleteTodoList(newTodoList);
     setTodoText("");
   };
+
+  const deleteTask = (index) => {
+    const newTodoList = [...incompleteTodoList];
+    newTodoList.splice(index, 1);
+    setIncompleteTodoList(newTodoList);
+  };
   return (
     <>
       <div className="c-task p-add_task">
@@ -36,12 +42,14 @@ const App = () => {
       <div className="c-task p-incomplete_task">
         <p className="c-title">未完了のTODO</p>
         <ul className="c-task_list">
-          {incompleteTodoList.map((todo) => {
+          {incompleteTodoList.map((todo, index) => {
             return (
               <li key={todo} className="c-task_list__item">
                 <p className="c-task_list__item_name">{todo}</p>
                 <button className="c-button">完了</button>
-                <button className="c-button">削除</button>
+                <button onClick={() => deleteTask(index)} className="c-button">
+                  削除
+                </button>
               </li>
             );
           })}
